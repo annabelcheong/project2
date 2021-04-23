365686de2ba53461809f1b9f823d2aadda402e8e
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-23 20:50:59 +0800

    fixed column headings

--- schema.sql
+++ schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE events_table (
   category INT NOT NULL,
   startDate VARCHAR NOT NULL,
   endDate VARCHAR NOT NULL,
-  country INT NOT NULL
-  coord DEC NOT NULL,
+  country INT NOT NULL,
+  coords DEC NOT NULL,
   rank INT NOT NULL,
   venue_name VARCHAR NOT NULL,
   formatted_address VARCHAR NOT NULL
