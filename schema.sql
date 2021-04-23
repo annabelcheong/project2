3a141c28244371aca36fae3c62cbfe9dc705469c
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-23 20:48:30 +0800

    updated column headings and schema.sql

--- schema.sql
+++ schema.sql
@@ -2,17 +2,16 @@
 -- TABLE 1 --
 --------------
 
-CREATE TABLE events_info (
+CREATE TABLE events_table (
   id VARCHAR PRIMARY KEY,  
   title VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
   category INT NOT NULL,
-  start DEC NOT NULL,
-  end INT NOT NULL,
+  startDate VARCHAR NOT NULL,
+  endDate VARCHAR NOT NULL,
   country INT NOT NULL
-  location DEC NOT NULL,
+  coord DEC NOT NULL,
   rank INT NOT NULL,
-  name VARCHAR NOT NULL,
   venue_name VARCHAR NOT NULL,
   formatted_address VARCHAR NOT NULL
 );
