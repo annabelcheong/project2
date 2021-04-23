c681c0191f884954942bb104e38e217369d90e91
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-23 22:25:55 +0800

    started app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, redirect
 # from flask_pymongo import PyMongo , CHANGE THIS TO POSGRES VERSION
+from flask_sqlalchemy import SQLAlchemy
 import events_flask
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use xxxxxxx to establish PostGRES connection
+# Connect to local database
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:postgres@localhost:5432/events_db"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# rds_connection_string = "postgres:postgres@localhost:5432/music_db"
+# engine = create_engine(f'postgresql://{rds_connection_string}')
 
 @app.route("/")
 def home():
-
-
-
+    #### NEED TO EDIT THE FOLLOWING ####
+    # mars_collect_data = mongo.db.mars_collect.find_one()
+    # return render_template("index.html", mars_dict = mars_collect_data)
 
 
 
