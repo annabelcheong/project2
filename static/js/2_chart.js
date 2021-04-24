371f635429e8b434411f999587bdc0377bcd1754
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-24 19:32:01 +0800

    unable to console log the title

--- static/js/2_chart.js
+++ static/js/2_chart.js
@@ -1,6 +1,8 @@
 function BuildBubbleChart() {
     d3.json("/api_events").then((data) => {
-        console.log(data);
+        // console.log(data);
+        console.log(data.map(elem => elem.title));
+        // console.log(data[0].title);
     });
 }
 
