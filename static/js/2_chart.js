e099212f44982a36e27607f114cc72d39cd0b655
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-25 11:27:19 +0800

    trialling console.log

--- static/js/2_chart.js
+++ static/js/2_chart.js
@@ -1,7 +1,18 @@
 
+// var obj = JSON.parse("/api_events")
+
 function BuildBubbleChart() {
     d3.json("/api_events").then((data) => {
+       
         console.log(data);
+        // console.log(hello.title);
+        console.log(data[16]);
+        console.log(data[17]);
+        console.log(data[18]);
+        console.log(data[19]);
+
+       
+        // console.log(data.title);
 
         // console.log(data.map(elem => elem.id));
         
@@ -11,6 +22,16 @@ function BuildBubbleChart() {
         // console.log(mappingArray);
     
     });
+
+    // d3.json('/api_events'), function(data) {
+
+    //     console.log(data);
+
+    // }
+
+
+
+
 }
 
 BuildBubbleChart();
