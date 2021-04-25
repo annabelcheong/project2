fd412a8f92ab4898100606c824d50af7d61b461a
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-04-25 15:28:02 +0800

    added calendar dropdown placeholder to html

--- static/js/3_table.js
+++ static/js/3_table.js
@@ -1,51 +1,55 @@
-// from data.js
-const tableData = data;
+var obj = JSON.parse("/api_events")
+// 'api_events'
 
 // get table references
 const tbody = d3.select("tbody");
 
-function buildTable(data) {
-  // First, clear out any existing data
-  tbody.html("");
+function BuildTable() {
+  d3.json('api_events').then((data) => {
+    console.log(data);
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    const row = tbody.append("tr");
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-        cell.text(val);
-      }
-    );
-  });
-}
+//   // First, clear out any existing data
+//   tbody.html("");
 
-function handleClick() {
+//   // Next, loop through each object in the data
+//   // and append a row and cells for each value in the row
+//   data.forEach((dataRow) => {
+//     // Append a row to the table body
+//     const row = tbody.append("tr");
 
-    // Grab the datetime value from the filter
-    const date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
+//     // Loop through each field in the dataRow and add
+//     // each value as a table cell (td)
+//     Object.values(dataRow).forEach((val) => {
+//       let cell = row.append("td");
+//         cell.text(val);
+//       }
+//     );
+//   });
+// }
+
+// function handleClick() {
+
+//     // Grab the datetime value from the filter
+//     const date = d3.select("#datetime").property("value");
+//     let filteredData = tableData;
   
-    // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
-    }
+//     // Check to see if a date was entered and filter the
+//     // data using that date.
+//     if (date) {
+//       // Apply `filter` to the table data to only keep the
+//       // rows where the `datetime` value matches the filter value
+//       filteredData = filteredData.filter(row => row.datetime === date);
+//     }
   
-    // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
-  }
+//     // Rebuild the table using the filtered data
+//     // @NOTE: If no date was entered, then filteredData will
+//     // just be the original tableData.
+//     buildTable(filteredData);
+//   }
   
-  // Attach an event to listen for the form button
-  d3.selectAll("#filter-btn").on("click", handleClick);
+//   // Attach an event to listen for the form button
+//   d3.selectAll("#filter-btn").on("click", handleClick);
   
-  // Build the table when the page loads
-  buildTable(tableData);
+//   // Build the table when the page loads
+//   buildTable(tableData);
