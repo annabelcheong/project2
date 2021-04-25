462de967082b60fa5360424ff3b7d563d3ef45bd
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-25 14:37:57 +0800

    assigned start and end date variables

--- static/js/2_chart.js
+++ static/js/2_chart.js
@@ -17,12 +17,18 @@ function BuildBubbleChart() {
         // console.log(data.map(elem => elem.title));
         title = data.map(elem => elem.title);
 
+        coords = data.map(elem => elem.coords);
+
         category = data.map(elem => elem.category);
 
         venue_name = data.map(elem => elem.venue_name);
 
         // Parse to numeric value
         rank = data.map((elem) => elem.rank = +elem.rank);
+
+        start_date = data.map(elem => elem.start_date);
+
+        end_date = data.map(elem => elem.end_date);
         
 
         
