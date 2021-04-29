f7fffd3251fdc962c1b76bcdc04697dc4104dd4b
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-04-29 14:30:47 +0800

    cleaned up 2_chart.js and added traces on initial load to cover all categories

--- static/js/2_chart.js
+++ static/js/2_chart.js
@@ -1,17 +1,8 @@
-
 function BuildBubbleChart() {
     
     d3.json('/api_events').then((data) => {
         // console.log(data);
         var allData = data;
-        
-        // id = data.map(elem => elem.id);
-        // coords = data.map(elem => elem.coords);
-
-        var title_name = data.map(elem => elem.title);
-
-        var category = data.map(elem => elem.category);
-        // console.log(category);
 
         ///////////////////////////
         // DROP DOWN FOR CATEGORY
@@ -35,45 +26,44 @@ function BuildBubbleChart() {
         d3.select("#selCategory").on("change", function(selectedOption) {
             
             var selectedOption = d3.select(this).property("value")
-            console.log(selectedOption); //This works
-            // console.log(data); // This works but unexpected data returned
-    
-            //console.log(title_name); // it reads
+            console.log(selectedOption); 
+          
             filtData = allData.filter(allData => allData.category == selectedOption);
             console.log(filtData);
 
-            // Run function updateBubbleChart with selectedOption
-            updateBubbleChart(selectedOption);
-            updateTable();
+            // RUN FUNCTIONS // 
+            updateBubbleChart(selectedOption); 
+            updateTable(); // The code for this function is located in 3_table.js file
 
         });
         
-
         ///////////////////////////
         // function UPDATEBUBBLECHART
         ///////////////////////////
 
         function updateBubbleChart(selectedGroup){
-            // console.log(selectedGroup); // This works. Prints out the selectedGroup, which is selectedOption
+            // console.log(selectedGroup); //  Prints out the selectedGroup, which is selectedOption
             
             //// Filtered array for category 'selectedGroup' (expos, community, performing-arts, sports etc)
-            //console.log(filtData); // This works. Prints array of info with categories filtered by selectedGroup
-            
-
-
-            // Add in bubble chart code
-            
+            //console.log(filtData); // Prints array of info with categories filtered by selectedGroup
+                        
             // x-axis variable for category
             cat_date = filtData.map(elem => elem.start_date = new Date(elem.start_date));
             //console.log(cat_date); //Print array of dates based on filtered selectedGroup array
 
             // y-axis variable for category
             cat_venue_name = filtData.map(elem => elem.venue_name);
-            //console.log(cat_venue_name); //Print array of venue names  based on filtered selectedGroup array
 
             // pop-up text (title required)
             cat_title_name = filtData.map(elem => elem.title);
-            
+
+            // Marker size
+            // Parse to numeric value '+'
+            // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+            // Multiplied values by 1000 to get it to a reasonable size on chart.
+            var rank_size = data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
+            //console.log(rank_size);
+                
             ///////////
             // DATA
             ///////////
@@ -87,8 +77,6 @@ function BuildBubbleChart() {
                 marker: {
                     size: rank_size,
                     color: "#119dff" // blue
-                    //rank, // Colours grouped by rank i.e. In this instance, grouped by size
-                    // colorscale: 'Portland'
                 },
                 name: selectedGroup // Legend
             };
@@ -101,7 +89,6 @@ function BuildBubbleChart() {
             ///////////
 
             var layout = {
-                // hovermode: "closest",
                 xaxis: { title: "Date", automargin: true },
                 yaxis: { title: "", automargin: true },
                 showlegend: true,
@@ -113,9 +100,17 @@ function BuildBubbleChart() {
             // Plot graph with plotly 
             Plotly.newPlot("bubble", data, layout); //'bubble' is the id reference in the html
 
-        };
+        }; // End of function updateBubbleChart
 
+        ///////////////////////////
+        // within function BuildBubbleChart().
+        // * Code for when page loads. 
+        ///////////////////////////
 
+        // SAVE VARIABLES FOR USE IN PLOTLY CHART'S X-AXIS, Y-AXIS, POP-UP //
+        
+        //////////////////////////////////
+        // DATA - EXPOS //
         //////////////////////////////////
         //// Filtered array for category 'expos'
         expo_data = data.filter(data => data.category == "expos");
@@ -130,8 +125,15 @@ function BuildBubbleChart() {
         // pop-up for category
         expo_title_name = expo_data.map(elem => elem.title);
 
-        ///////////////////////////////////
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var expo_rank_size = expo_data.map((elem) => elem.rank = 1/(+elem.rank)*1500);
+        //console.log(rank_size);
 
+        ///////////////////////////////////
+        // DATA - CONFERENCES //
         //////////////////////////////////
         //// Filtered array for category 'conferences'
         conf_data = data.filter(data => data.category == "conferences");
@@ -145,6 +147,15 @@ function BuildBubbleChart() {
         // pop-up for category
         conf_title_name = conf_data.map(elem => elem.title);
 
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var conf_rank_size = conf_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
+
+
+        //////////////////////////////////
+        // DATA - COMMUNITY //
         //////////////////////////////////
         //// Filtered array for category 'community'
         comm_data = data.filter(data => data.category == "community");
@@ -158,21 +169,40 @@ function BuildBubbleChart() {
         // pop-up for category
         comm_title_name = comm_data.map(elem => elem.title);
 
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var comm_rank = comm_data.map((elem) => elem.rank = +elem.rank);
+        console.log(comm_rank);
+        var comm_rank_size = comm_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
+
+
 
         //////////////////////////////////
+        // DATA - SPORTS //
+        //////////////////////////////////
         //// Filtered array for category 'sports'
-        sports_data = data.filter(data => data.category == "sports");
+        var sports_data = data.filter(data => data.category == "sports");
 
         // x-axis variable for category
-        sports_date = sports_data.map(elem => elem.start_date = new Date(elem.start_date))
+        var sports_date = sports_data.map(elem => elem.start_date = new Date(elem.start_date))
         
         // y-axis variable for category
-        sports_venue_name = sports_data.map(elem => elem.venue_name);
+        var sports_venue_name = sports_data.map(elem => elem.venue_name);
 
         // pop-up for category
-        sports_title_name = sports_data.map(elem => elem.title);
+        var sports_title_name = sports_data.map(elem => elem.title);
+
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var sports_rank_size = sports_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
 
 
+        //////////////////////////////////
+        // DATA - PERFORMING ARTS //
         //////////////////////////////////
         //// Filtered array for category 'performing-arts'
         perf_data = data.filter(data => data.category == "sports");
@@ -185,7 +215,16 @@ function BuildBubbleChart() {
 
         // pop-up for category
         perf_title_name = perf_data.map(elem => elem.title);
+
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var perf_rank_size = perf_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
         
+
+        //////////////////////////////////
+        // DATA - FESTIVALS //
         //////////////////////////////////
         //// Filtered array for category 'festivals'
         fest_data = data.filter(data => data.category == "festivals");
@@ -199,6 +238,15 @@ function BuildBubbleChart() {
         // pop-up for category
         fest_title_name = fest_data.map(elem => elem.title);
 
+        // Marker size
+        // Parse to numeric value
+        // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
+        // Multiplied values by 1000 to get it to a reasonable size on chart.
+        var fest_rank_size = fest_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
+
+
+        //////////////////////////////////
+        // DATA - CONCERTS //
         //////////////////////////////////
         //// Filtered array for category 'concerts'
         conc_data = data.filter(data => data.category == "concerts");
@@ -212,30 +260,13 @@ function BuildBubbleChart() {
         // pop-up for category
         conc_title_name = conc_data.map(elem => elem.title);
 
-
-
-
-        
-        var venue_name = data.map(elem => elem.venue_name);
-
+        // Marker size
         // Parse to numeric value
-        var rank = data.map((elem) => elem.rank = +elem.rank);
-        //console.log(rank); //This works and gives back an array of numerical values
-
         // Inversed the values to allow rank of 1 to be biggest in marker size, 100 to be smallest. 
         // Multiplied values by 1000 to get it to a reasonable size on chart.
-        var rank_size = data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
-        //console.log(rank_size);
-        
+        var conc_rank_size = conc_data.map((elem) => elem.rank = 1/(+elem.rank)*1000);
 
 
-        var start_date = data.map((elem) => elem.start_date = new Date(elem.start_date));
-        //    console.log(start_date);
-
-
-        // var inverse_size = 100-rank;
-        // // inverse_size = +inverse_size;
-        // console.log(inverse_size);
 
         //////////////
         // TRACES //
@@ -245,13 +276,11 @@ function BuildBubbleChart() {
             x: expo_date,
             y: expo_venue_name,
             text: expo_title_name,  
-            // hover_info: rank,
             mode: 'markers',
             marker: {
-                size: rank_size, 
+                size: expo_rank_size, 
+                opacity: 0.7,
                 color: "#119dff" // blue
-                //rank, // Colours grouped by rank i.e. In this instance, grouped by size
-                // colorscale: 'Portland'
             },
             name: 'expos'
         };
@@ -262,10 +291,9 @@ function BuildBubbleChart() {
             text: conf_title_name,  
             mode: 'markers',
             marker: {
-                size: rank_size, 
+                size: conf_rank_size, 
+                opacity: 0.7,
                 color: "#ff870f" // orange
-                //'rgb(17, 157, 255)'//,rank, // Colours grouped by rank i.e. In this instance, grouped by size
-                // colorscale: 'Portland'
             },
             name: 'conferences'
         };
@@ -276,15 +304,66 @@ function BuildBubbleChart() {
             text: comm_title_name,  
             mode: 'markers',
             marker: {
-                size: rank_size, 
+                size: comm_rank_size, 
+                opacity: 0.7,
                 color: "#33cc33" // green
-                //rank, // Colours grouped by rank i.e. In this instance, grouped by size
-                // colorscale: 'Portland'
             },
             name: 'community'
         };
 
-        var data = [trace1, trace2, trace3];
+        var trace4 = {
+            x: sports_date,
+            y: sports_venue_name,
+            text: sports_title_name,  
+            mode: 'markers',
+            marker: {
+                size: sports_rank_size, 
+                opacity: 0.7,
+                color: "#cc0099" // fuscia
+            },
+            name: 'sports'
+        };
+
+        var trace5 = {
+            x: perf_date,
+            y: perf_venue_name,
+            text: perf_title_name,  
+            mode: 'markers',
+            marker: {
+                size: perf_rank_size, 
+                opacity: 0.7,
+                color: "#ffff1a" // yellow
+            },
+            name: 'performing arts'
+        };
+
+        var trace6 = {
+            x: fest_date,
+            y: fest_venue_name,
+            text: fest_title_name,  
+            mode: 'markers',
+            marker: {
+                size: fest_rank_size, 
+                opacity: 0.7,
+                color: "#99ffff" // baby blue
+            },
+            name: 'festivals'
+        };
+
+        var trace7 = {
+            x: conc_date,
+            y: conc_venue_name,
+            text: conc_title_name,  
+            mode: 'markers',
+            marker: {
+                size: conc_rank_size, 
+                opacity: 0.7,
+                color: "#ffccff" // baby pink
+            },
+            name: 'concerts'
+        };
+
+        var data = [trace1, trace2, trace3, trace4, trace5, trace7];
 
         //////////////
         // LAYOUT //
